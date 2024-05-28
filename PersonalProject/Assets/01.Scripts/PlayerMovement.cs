89a59f82202a566230171e832a78b16f62e05cc3
gksdnfgksdnf <kkho8489@gmail.com>
2024-05-29 08:35:46 +0900

    PlayerRotation & PlayerCollider

--- PersonalProject/Assets/01.Scripts/PlayerMovement.cs
+++ PersonalProject/Assets/01.Scripts/PlayerMovement.cs
@@ -6,12 +6,15 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
     private PlayerInput _playerInput;
-    private Vector3 targetPosition;
+
     [SerializeField] float _speed = 5f;
 
     private bool _isMove = false;
+
+    private Vector3 targetPosition;
+    private Vector3 _movement;
+    private Quaternion _targetRotation;
 
     private void Awake()
     {
@@ -26,21 +29,42 @@ public class PlayerMovement : MonoBehaviour
     {
         if (!_isMove)
         {
+            _movement = movement;
             targetPosition = transform.position + movement;
             _isMove = true;
         }
     }
 
+    private void ApplyRotation(Vector3 movement)
+    {
+        float rotationSpeed = 17f;
+        _targetRotation = Quaternion.LookRotation(movement);
+        transform.rotation = Quaternion.Lerp(
+                            transform.rotation,
+                            _targetRotation,
+                            Time.fixedDeltaTime * rotationSpeed);
+    }
+
     private void FixedUpdate()
+    {
+        Movement();
+    }
+
+    private void Movement()
     {
         if (_isMove)
         {
             transform.position = Vector3.MoveTowards(transform.position, targetPosition, _speed * Time.fixedDeltaTime);
 
             if (transform.position == targetPosition)
-            {
                 _isMove = false;
-            }
         }
+        if (_movement != Vector3.zero)
+            ApplyRotation(_movement);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print(other.gameObject.name);
     }
 }
